e8c1d3b035a27c20529efdf6034a2ae5f098c089
Jacob Walser <jwalser90@gmail.com>
2018-04-08 01:47:06 -0400

    Add circular rx/tx buffers

--- uart.h
+++ uart.h
@@ -14,11 +14,16 @@ public:
 
 	void write(char* p, uint16_t len);
 	void write(char* p);
+
+	int read(void);
+	uint8_t rxWaiting(void);
+
 	void cls(void);
 	void bkspc(void);
-	char read();
 	void ITConfig(uint32_t it, FunctionalState enabled);
 
+	uint8_t txSpaceUsed(void);
+	uint8_t txSpaceAvailable(void);
 	void init(uint32_t baudrate,
 				uint32_t stopbits = USART_StopBits_1,
 				uint32_t wordlength = USART_WordLength_8b,
@@ -57,8 +62,21 @@ public:
 
 	void setClockEnabled(FunctionalState enabled);
 
-private:
+	static const uint8_t bufSize = 64;
+	char rxBuf[bufSize];
+	char txBuf[bufSize];
+
+	uint8_t rxHead;
+	uint8_t rxTail;
+	uint8_t rxOverruns;
+
+	uint8_t txHead;
+	uint8_t txTail;
+	uint8_t txOverruns;
+
 	USART_TypeDef* _peripheral;
+
+private:
 
 	USART_InitTypeDef _config;
 };
@@ -79,10 +97,25 @@ void Uart::ITConfig(uint32_t it, FunctionalState enabled)
 	USART_ITConfig(_peripheral, it, enabled);
 }
 
+uint8_t Uart::txSpaceUsed(void)
+{
+	return ((uint8_t)(txTail - txHead) % bufSize);
+}
+
+uint8_t Uart::txSpaceAvailable(void)
+{
+	return bufSize - txSpaceUsed() - 1;
+}
+
 void Uart::write(char* ch) {
-	USART_SendData(USART1, *ch);
-	uint32_t tstart = MicroSeconds;
-	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET && (MicroSeconds < tstart + 50));
+	while (!txSpaceAvailable()) {
+		txOverruns++; // block when buffer is full
+	}
+
+	txBuf[txTail++] = *ch;
+	txTail = txTail % bufSize;
+
+	_peripheral->CR1 |= USART_CR1_TXEIE;
 }
 
 void Uart::write(char* ch, uint16_t len)
@@ -92,7 +125,23 @@ void Uart::write(char* ch, uint16_t len)
 	}
 }
 
-void Uart::cls(void) {
+uint8_t Uart::rxWaiting(void)
+{
+	return ((uint8_t)(rxTail - rxHead) % bufSize);
+}
+
+int Uart::read(void)
+{
+	if (!rxWaiting()) {
+		return -1;
+	}
+	uint8_t bufIndex = rxHead++;
+	rxHead = rxHead % bufSize;
+	return rxBuf[bufIndex];
+}
+
+void Uart::cls(void)
+{
 	static const uint8_t esc = 27;
 	write((char*)(&esc));       // ESC command
 	printf("[2J");    // clear screen command
@@ -100,7 +149,8 @@ void Uart::cls(void) {
 	printf("[H"); // goto home
 }
 
-void Uart::bkspc(void) {
+void Uart::bkspc(void)
+{
 	static const uint8_t bkspc = 0x08;
 	write((char*)(&bkspc));       // ESC command
 }
