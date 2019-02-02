64d56bac5528963e62aace2bb6dbd53578116a3d
Jacob Walser <jwalser90@gmail.com>
2019-02-02 01:23:22 -0500

    startup stm32f350: update header

--- src/startup/startup_stm32f350.s
+++ src/startup/startup_stm32f350.s
@@ -1,38 +1,7 @@
-/**
-  ******************************************************************************
-  * @file      startup_stm32f30x.s
-  * @author    MCD Application Team
-  * @version   V1.2.2
-  * @date      27-February-2015
-  * @brief     stm32f30x vector table for Atollic TrueSTUDIO toolchain.
-  *    This module performs:
-  *        - Set the initial SP
-  *                - Set the initial PC == Reset_Handler,
-  *                - Set the vector table entries with the exceptions ISR address,
-  *                - Configure the clock system  
-  *                - Branches to main in the C library (which eventually
-  *                  calls main()).
-  *            After Reset the Cortex-M4 processor is in Thread mode,
-  *            priority is Privileged, and the Stack is set to Main.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2015 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+/*
+  startup file for giga device gd32f350 mcu
+  adapted from stm32 stuff
+ */
 
   .syntax unified
 	.cpu cortex-m4
