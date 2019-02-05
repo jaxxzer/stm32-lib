293c1da21b020929500fde452b9995ad20d2c6b7
Jacob Walser <jwalser90@gmail.com>
2019-02-04 22:27:58 -0500

    example-adc: update example to use 3 channels, 115kbps

--- src/example/example-adc.cpp
+++ src/example/example-adc.cpp
@@ -1,5 +1,5 @@
 #include "stm32lib-conf.h"
-#define USART_BAUDRATE 1e6
+#define USART_BAUDRATE 115200
 
 Gpio gpioLed{GPIO_LED1_PORT, GPIO_LED1_PIN};
 
@@ -133,15 +133,19 @@ int main() {
 #endif
   printf("starting\r\n");
   Adc adc1{ADC1};
-  AdcChannel *adcChan1;
-  adcChan1 = adc1.addChannel(ADC_CHAN);
+
+  AdcChannel *adcChan0 = adc1.addChannel(ADC_Channel_0);
+  AdcChannel *adcChan1 = adc1.addChannel(ADC_Channel_1);
+  AdcChannel *adcChan4 = adc1.addChannel(ADC_Channel_4);
+
   adc1.init();
   adc1.enable();
   adc1.startConversion();
 
   while (1) {
     adc1.waitConversion();
-    printf("adc1: %d\r\n", adcChan1->average);
+    printf("adc: %d\t%d\t%d\r\n", adcChan0->average, adcChan1->average, adcChan4->average);
+    mDelay(adcChan0->average / 4);
     gpioLed.toggle();
   }
 
