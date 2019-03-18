4e474268fb06c6f10712ecf3425c7d52dadc6c67
Jacob Walser <jwalser90@gmail.com>
2019-03-18 00:07:42 -0400

    stm32lib-conf: add bit operation macros

--- src/stm32lib-conf.h
+++ src/stm32lib-conf.h
@@ -11,6 +11,14 @@
 #error
 #endif
 
+/* bit operations */
+#define REG32(addr)                  (*(volatile uint32_t *)(uint32_t)(addr))
+#define REG16(addr)                  (*(volatile uint16_t *)(uint32_t)(addr))
+#define REG8(addr)                   (*(volatile uint8_t *)(uint32_t)(addr))
+#define BIT(x)                       ((uint32_t)((uint32_t)0x01U<<(x)))
+#define BITS(start, end)             ((0xFFFFFFFFUL << (start)) & (0xFFFFFFFFUL >> (31U - (uint32_t)(end))))
+#define GET_BITS(regval, start, end) (((regval) & BITS((start),(end))) >> (start))
+
 typedef struct it_callback {
   void (*callback)(void);
   it_callback *next = nullptr;
